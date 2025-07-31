aca87ce3a0e975af876607183fa0c8a6d196876f
grassblock <i@gb0.dev>
2025-07-31 13:04:25 +0800

    fix: actions NoneType error

--- core/actions.py
+++ core/actions.py
@@ -8,7 +8,7 @@ async def handle_actions(message: Message) -> None:
         return
     rawtext = message.text
     from_user = message.from_user.mention_html(message.sender_chat.title) if message.sender_chat else message.from_user.mention_html()
-    replied_user = message.from_user.mention_html(message.reply_to_message.sender_chat.title) if message.reply_to_message and message.reply_to_message.sender_chat else message.reply_to_message.from_user.mention_html()
+    replied_user = message.reply_to_message.from_user.mention_html(message.reply_to_message.sender_chat.title) if message.reply_to_message and message.reply_to_message.sender_chat else message.reply_to_message.from_user.mention_html()
     # 防止识别成命令而被误触发
     if rawtext.replace('/','',1).isascii() or '@' in rawtext:
         print(rawtext.replace('/','',1).isascii())
@@ -22,7 +22,7 @@ async def handle_actions(message: Message) -> None:
 
 async def handle_reverse_actions(message: Message) -> None:
     from_user = message.from_user.mention_html(message.sender_chat.title) if message.sender_chat else message.from_user.mention_html()
-    replied_user = message.from_user.mention_html(message.reply_to_message.sender_chat.title) if message.reply_to_message and message.reply_to_message.sender_chat else message.reply_to_message.from_user.mention_html()
+    replied_user = message.reply_to_message.from_user.mention_html(message.reply_to_message.sender_chat.title) if message.reply_to_message and message.reply_to_message.sender_chat else message.reply_to_message.from_user.mention_html()
     if not config.is_feature_enabled('actions', message.chat.id):
         return
     await message.reply(f"{from_user} 被 {replied_user if message.reply_to_message else '自己'} {message.text.replace('\\','')}了！",disable_web_page_preview=True)
